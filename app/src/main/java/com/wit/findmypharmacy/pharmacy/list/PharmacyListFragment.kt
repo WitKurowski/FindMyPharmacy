5271e7042b31bd7215da7f5a33365a8fe83fa76e
Wit Kurowski <witkurowski@gmail.com>
2022-08-14 13:23:55 -0700

    Renamed unneeded views from "fragment_pharmacy_list".

--- app/src/main/java/com/wit/findmypharmacy/pharmacy/list/PharmacyListFragment.kt
+++ app/src/main/java/com/wit/findmypharmacy/pharmacy/list/PharmacyListFragment.kt
@@ -5,8 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.wit.findmypharmacy.R
 import com.wit.findmypharmacy.databinding.FragmentPharmacyListBinding
 
 class PharmacyListFragment : Fragment() {
@@ -26,13 +24,5 @@ class PharmacyListFragment : Fragment() {
 		super.onDestroyView()
 
 		_binding = null
-	}
-
-	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-		super.onViewCreated(view, savedInstanceState)
-
-		binding.buttonFirst.setOnClickListener {
-			findNavController().navigate(R.id.action_PharmacyListFragment_to_SecondFragment)
-		}
 	}
 }
